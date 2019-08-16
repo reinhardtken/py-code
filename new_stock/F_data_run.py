bf7dffbf73038d1bca54f8f9ed31f9d72a4e82e3
leivenwong <13852904@qq.com>
2019-08-16 11:05:54 +0800

    modify main and F_data_run

--- new_stock/F_data_run.py
+++ new_stock/F_data_run.py
@@ -1,6 +1,13 @@
-import F_data_to_db
-import F_data_out
 
 if __name__ == '__main__':
+    import F_data_to_db
+    import F_data_out
+    # use it to create stockList and hs300 when the first time and mongodb is empty
+    import fake_spider.tushare.hs300
+    fake_spider.tushare.hs300.saveDB(fake_spider.tushare.hs300.getHS300())
+    import fake_spider.tushare.stockList
+    fake_spider.tushare.stockList.saveDB(fake_spider.tushare.stockList.getBasics())
+
+    #run main program
     F_data_to_db.run()
     F_data_out.run()
